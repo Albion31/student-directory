91cbf845cf1e4477163b60e648125b48aef50ddc
Lan Pham <lanpham312@gmail.com>
2017-11-09 12:54:43 +0000

    Loading the data from the student.csv file

--- directory.rb
+++ directory.rb
@@ -11,7 +11,8 @@ end
 def print_menu
   puts "1. Input the students."
   puts "2. Show the students."
-  puts "3. Save the list to students.csv"
+  puts "3. Save the list to students.csv."
+  puts "4. Load the list from students.csv."
   puts "9. Exit."
 end
 
@@ -23,6 +24,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -47,6 +50,21 @@ def save_students
                     student[:colour]]
     csv_line = student_data.join(",")
     file.puts(csv_line)
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, country, hobby, height, colour = line.chomp.split(",")
+    @students << {name: name,
+                  cohort: cohort.to_sym,
+                  country: country,
+                  hobby: hobby,
+                  height: height,
+                  colour: colour
+                 }
   end
   file.close
 end
